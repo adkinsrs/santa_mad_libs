d00a47216155de5df07eb047e577742f4ff2da50
adkinsrs <shaun.adkins@gmail.com>
2017-11-17 20:28:32 -0500

    starting some initial Flask-WTF work

--- app/app.py
+++ app/app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from flask import Flask
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
 import mad_libs
 
 app = Flask(__name__)
@@ -8,6 +11,11 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+### Non-routing functions
+
+def choose_random_mad_lib():
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
