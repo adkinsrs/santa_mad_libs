d5dd5642f8cbe0d480f3ab3faceadc4e6935cf52
adkinsrs <shaun.adkins@gmail.com>
2017-11-17 20:21:28 -0500

    initial commit

---
+++ app/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from flask import Flask
+import mad_libs
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
---
+++ app/mad_libs.py
@@ -0,0 +1,3 @@
+# Will house the list of Mad Libs that will be picked from random
+
+mad_libs = {}
