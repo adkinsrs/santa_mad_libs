c780184aa48bad010dfadbb4cf00d1f5a915e1c1
adkinsrs <shaun.adkins@gmail.com>
2017-12-14 18:09:47 -0500

    added naughty and nice categories

--- app/app.py
+++ app/app.py
@@ -11,6 +11,19 @@ app = Flask(__name__)
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # Was 'Nice' or 'Naughty' clicked?
+        if "nice" in request.form:
+            status = 'nice'
+        else:
+            status = 'naughty'
+        return render_template(url_for('mad_libs_form'), status=status)
+
+    else:
+        return render_template("home.html")
+
+@app.route('/mad_libs_form', methods=['GET', 'POST'])
+def mad_libs_form(status):
+    if request.method == 'POST':
         story = request.form['story']
 
         # If story is in mad_libs dict correctly, the story list should be 1 element longer than the fill_ins list
@@ -24,18 +37,31 @@ def index():
         final_story += story_list[-1]
         # Unicode is being encoded to ASCII and I'm getting errors if I don't ignore the chars that can't be encoded in ASCII
         final_story = final_story.encode('ascii', 'ignore')
-        return '<p style="font-size:150%;">{}</p>'.format(final_story)
+        return render_template(url_for('final'), story=final_story)
     else:
-        (story, fill_ins) = choose_random_mad_lib()
-        return render_template('mad_libs_form.html', form_list=fill_ins, story=story)
+        if status == 'nice':
+            (story, fill_ins) = choose_random_mad_lib(ml.nice_mad_libs)
+            return render_template('nice_mad_libs.html', form_list=fill_ins, story=story)
+        else:
+            (story, fill_ins) = choose_random_mad_lib(ml.naughty_mad_libs)
+            return render_template('naughty_mad_libs.html', form_list=fill_ins, story=story)
+
+@app.route('/final', methods=['GET', 'POST'])
+def final(story):
+    if request.method == 'POST':
+        if "start_over" in request.form:
+            return render_template(url_for('index'))
+    else:
+        return render_template("final.html", story=story)
+
 
 ### Non-routing functions
 
 def break_story_into_list(story):
     return story.split(ml.FILL_IN_PTRN)
 
-def choose_random_mad_lib():
-    return random.choice( list(ml.mad_libs.iteritems()) )
+def choose_random_mad_lib(mad_libs):
+    return random.choice( list(mad_libs.iteritems()) )
 
 
 if __name__ == "__main__":
